6aef9210f614919e878d65b0bce4e9c2bf838024
lovixk <991807272@qq.com>
2022-09-14 06:23:35 +0800

    func

--- func/func.go
+++ func/func.go
@@ -26,10 +26,43 @@ func Test4() {
 	fmt.Println("函数名首字母大写则对所有包可见")
 	fmt.Println("函数名首字母小写则对当前包可见")
 }
+
+/*
+多个参数类型不一致
+当参数的类型不一致时，可以使用 interface 代替
+*/
+func test5(args ...interface{}) {
+	for _, value := range args {
+		switch value.(type) {
+		case int:
+			fmt.Println(value, "type is int")
+			break
+		case float32:
+			fmt.Println(value, "type is float32")
+			break
+		case float64:
+			fmt.Println(value, "type is float64")
+			break
+		case bool:
+			fmt.Println(value, "type is bool")
+			break
+		case string:
+			fmt.Println(value, "type is string")
+			break
+		case nil:
+			fmt.Println(value, "type is nil")
+			break
+		default:
+			fmt.Println(value, "type is unknown")
+			break
+		}
+	}
+}
 func main() {
 	fmt.Println(test(1, 2))
 	fmt.Println(test2(1, 2, 3, 4, 5, 6, 7))
 	var s []string
 	s = append(s, []string{"hello", "world"}...)
 	test3(s...)
+	test5(1, "string", false)
 }
